001b0e352b4f195e057d117c648c0659778f3a58
udaraliyanage96cs <61611899+udaraliyanage96cs@users.noreply.github.com>
2024-06-05 17:28:43 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const cors = require('cors');
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USERNAME, 
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+app.post('/send-email', (req, res) => {
+    const { to, text , key } = req.body;
+  
+    if (!to || !text || !key) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+  
+    let mailOptions = {
+      from: `"CabCeylon" <${process.env.MAIL_FROM_ADDRESS}>`, 
+      to: [to,'cabceylon02@gmail.com'], 
+      subject: 'CabCeylon Vehicle Booking',
+      text: text,
+    };
+  
+    if(key == process.env.KEY){
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.log(error);
+              return res.status(500).send('Failed to send email');
+            } else {
+              console.log('Message sent: %s', info.messageId);
+              return res.send('Email sent successfully');
+            }
+        });
+    }else{
+        return res.status(500).send('Invalid KEY');
+    }
+    
+  });
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

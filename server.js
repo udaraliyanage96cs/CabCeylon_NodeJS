614b5ca3903b505d30e2ebce06fc375924c1d468
Udara Liyanage <61611899+udaraliyanage96cs@users.noreply.github.com>
2024-06-09 21:47:07 +0530

    Contact Email Send

--- server.js
+++ server.js
@@ -20,34 +20,82 @@ let transporter = nodemailer.createTransport({
 });
 
 app.post('/send-email', (req, res) => {
-    const { to, html , key } = req.body;
+  const { to, html , key } = req.body;
+
+  if (!to || !html || !key) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  let mailOptions = {
+    from: `"CabCeylon" <${process.env.MAIL_FROM_ADDRESS}>`, 
+    to: [to,'cabceylon02@gmail.com'], 
+    subject: 'CabCeylon Vehicle Booking',
+    html: html,
+  };
+
+  if(key == process.env.KEY){
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            return res.status(500).send('Failed to send email');
+          } else {
+            console.log('Message sent: %s', info.messageId);
+            return res.send('Email sent successfully');
+          }
+      });
+  }else{
+      return res.status(500).send('Invalid KEY');
+  }
   
-    if (!to || !html || !key) {
-      return res.status(400).json({ error: 'Missing required fields' });
-    }
+});
+
+app.post('/send-contact-email', (req, res) => {
+  const { to, emailHtmlClient , emailHtmlAdmin, submission, key } = req.body;
+
+  if (!to || !emailHtmlClient || !emailHtmlAdmin ||  !submission || !key) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  let mailOptions = {
+    from: `"CabCeylon" <${process.env.MAIL_FROM_ADDRESS}>`, 
+    to: [to], 
+    subject: 'Contact Form Submission',
+    html: emailHtmlClient,
+  };
+
+  let mailOptionAdmin = {
+    from: `"CabCeylon" <${process.env.MAIL_FROM_ADDRESS}>`, 
+    to: ['cabceylon02@gmail.com'], 
+    subject: 'Contact Form Submission',
+    html: emailHtmlAdmin,
+  };
+
+  if(key == process.env.KEY){
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            return res.status(500).send('Failed to send email');
+          } else {
+            console.log('Message sent: %s', info.messageId);
+            return res.send('Email sent successfully');
+          }
+      });
+
+      transporter.sendMail(mailOptionAdmin, (error, info) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).send('Failed to send email');
+        } else {
+          console.log('Message sent: %s', info.messageId);
+          return res.send('Email sent successfully');
+        }
+    });
+
+  }else{
+      return res.status(500).send('Invalid KEY');
+  }
   
-    let mailOptions = {
-      from: `"CabCeylon" <${process.env.MAIL_FROM_ADDRESS}>`, 
-      to: [to,'cabceylon02@gmail.com'], 
-      subject: 'CabCeylon Vehicle Booking',
-      html: html,
-    };
-  
-    if(key == process.env.KEY){
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-              return res.status(500).send('Failed to send email');
-            } else {
-              console.log('Message sent: %s', info.messageId);
-              return res.send('Email sent successfully');
-            }
-        });
-    }else{
-        return res.status(500).send('Invalid KEY');
-    }
-    
-  });
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
